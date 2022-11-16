39c40e08b0f56d873e4f895f52772123393a287e
rohithmsr <rohithmsr2001@gmail.com>
2022-11-16 20:32:18 +0530

    fix: Change .env file path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({
-    path: './config.env'
+  path: './.env',
 });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
+  console.log(`App running on port ${port}`);
+});
