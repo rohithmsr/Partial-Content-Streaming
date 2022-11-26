6179505637bdb0f9d353b980e53224b421c01f6f
rohithmsr <rohithmsr2001@gmail.com>
2022-11-26 19:31:25 +0530

    fix: Security Practices

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const streamRouter = require('./routes/streamRoutes');
 const songRouter = require('./routes/songRoutes');
@@ -9,14 +13,40 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+// Security HTTP Headers
+app.use(helmet());
+
+// Rate Limit Header
+// Requests per ip at amount of time!
+// 5000 requests from same ip in 1 hour
+const limiter = rateLimit({
+  max: 5000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body to req.body
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+// removes operators and dollar signs
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// Prevent HTML code + JS code injection! Convert them into characters!
+app.use(xss());
+
+// Serving static files
 app.use(express.static(path.join(__dirname, 'assets')));
 
+// Time adding middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// Routers
 app.use('/api/v1/streams', streamRouter);
 app.use('/api/v1/songs', songRouter);
 app.use('/api/v1/users', userRouter);
@@ -27,6 +57,7 @@ app.get('/api/v1/', (req, res) => {
   });
 });
 
+// Error handlers
 app.all('*', (req, res, next) => {
   // const err = new Error();
   // err.status = 'fail';
